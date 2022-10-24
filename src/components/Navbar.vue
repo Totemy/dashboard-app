2ba6d5098eafccbabade2a4d93a0ce55a9198ed9
Totemy <thetotemofficial@gmail.com>
2022-10-24 23:11:09 +0300

    Editing navbar items to pixel perfect

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,24 +7,32 @@
           <span class="navigation__version">v.01</span>
         </h2>
       </div>
-      <div class=" navigation__item">
-        <span class="icon icon-dashboard navigation__icon"></span> Dashboard
+      <div class="navigation__items">
+        <div class=" navigation__item">
+          <span class="icon icon-dashboard navigation__icon"></span> Dashboard
+        </div>
+        <div class=" navigation__item">
+          <span class="icon icon-product navigation__icon"></span> Product
+          <span class="icon icon-chevron navigation__icon"></span>
+        </div>
+        <div class=" navigation__item navigation__item_active">
+          <span class="icon icon-customers navigation__icon"></span> Customers
+          <span class="icon icon-chevron-active navigation__icon"></span>
+        </div>
+        <div class=" navigation__item">
+          <span class="icon icon-income navigation__icon"></span> Income
+          <span class="icon icon-chevron navigation__icon"></span>
+        </div>
+        <div class=" navigation__item">
+          <span class="icon icon-promote navigation__icon"></span> Promote
+          <span class="icon icon-chevron navigation__icon"></span>
+        </div>
+        <div class=" navigation__item">
+          <span class="icon icon-help navigation__icon"></span> Help
+          <span class="icon icon-chevron navigation__icon"></span>
+        </div>
       </div>
-      <div class=" navigation__item">
-        <span class="icon icon-product navigation__icon"></span> Product
-      </div>
-      <div class=" navigation__item navigation__item_active">
-        <span class="icon icon-customers navigation__icon"></span> Customers
-      </div>
-      <div class=" navigation__item">
-        <span class="icon icon-income navigation__icon"></span> Income
-      </div>
-      <div class=" navigation__item">
-        <span class="icon icon-promote navigation__icon"></span> Promote
-      </div>
-      <div class=" navigation__item">
-        <span class="icon icon-help navigation__icon"></span> Help
-      </div>
+
       <div class="profile">
         <div class="profile__img">
           <img src="images/navbar/avatar.png" />
