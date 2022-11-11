ee65366c05214758dec052d475261212f1fb7a3f
Totemy <thetotemofficial@gmail.com>
2022-11-11 19:49:02 +0200

    Added dashboard and chart

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,3 +1,50 @@
 <template>
-  <div>same</div>
+  <div class="container">
+    <canvas id="test-chart"></canvas>
+  </div>
 </template>
+<script>
+import Chart from "chart.js/auto";
+export default {
+  mounted() {
+    var ctx = document.getElementById("test-chart");
+    var myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: [
+          "Red",
+          "Blue",
+          "Yellow",
+          "Green",
+          "Purple",
+          "Orange",
+          "1",
+          "2",
+          "3"
+        ],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3, 5, 6, 7],
+            backgroundColor: ["rgba(255, 159, 64, 0.2)"],
+            borderColor: ["rgba(255, 159, 64, 1)"],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
+    myChart;
+  }
+};
+</script>
